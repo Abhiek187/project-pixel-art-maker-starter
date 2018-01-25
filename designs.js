31a94782cc06aa6405fa5f2836db7f1c1909f66f
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2018-01-25 17:43:27 -0500

    Add all functionality for project

--- designs.js
+++ designs.js
@@ -1,10 +1,45 @@
-// Select color input
-// Select size input
+$(document).ready(function() {
+	// Select color input
+	let gridColor = '#000000';
 
-// When size is submitted by the user, call makeGrid()
+	$('#colorPicker').change(function() {
+		gridColor = $(this).val();
+	});
 
-function makeGrid() {
+	$('#pixel_canvas').click(function(event) {
+		$(event.target).css('background-color', gridColor); // color only the tile the user clicked on
+	});
 
-// Your code goes here!
+	// Select size input
+	let gridHeight = $('#input_height').val();
+	let gridWidth = $('#input_width').val();
 
-}
+	$('#input_height').change(function() {
+		gridHeight = $(this).val();
+	});
+
+	$('#input_width').change(function() {
+		gridWidth = $(this).val();
+	});
+
+	// When size is submitted by the user, call makeGrid()
+	$('#button_submit').click(function(event) {
+		makeGrid();
+		event.preventDefault(); // prevents console from reseting
+	});
+
+	function makeGrid() {
+		// Your code goes here!
+		$('tr').remove(); // clear the grid
+
+		for (let r = 0; r < gridHeight; r++) {
+			$('#pixel_canvas').append('<tr>');
+
+			for (let c = 0; c < gridWidth; c++) {
+				$('tr').last().append('<td></td>'); // always insert data at the last <tr>
+			}
+
+			$('#pixel_canvas').append('</tr>');
+		}
+	}
+});
